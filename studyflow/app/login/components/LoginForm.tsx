b3a5b819793a9dc76799e0d2ca25b52638bc184f
Aleksandar Radojicic <aleksandarradojicic0@gmail.com>
2026-05-30 19:50:25 +0200

    Login Page Changes
    -Login Page Design Finished
    -Added Login Form
    -Thinking about turning custom inputs into components.

--- studyflow/app/login/components/LoginForm.tsx
+++ studyflow/app/login/components/LoginForm.tsx
@@ -1,10 +1,41 @@
+import { arrowRight, lockIcon, mailIcon } from "~/constants";
 
 
 export function LoginForm(){
 
     return(
-        <form action="">
-            
+        <form className="loginForm">
+            <div className="flex flex-col gap-6.25 w-full">
+
+                <div>
+                    <div className="flex justify-between">
+                        <p className="formInputName">Email Address</p>
+                    </div>
+                    <div className="customInput">
+                        {mailIcon}
+                        <input type="text" name="email" placeholder="name@example.com"/>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex justify-between">
+                        <p className="formInputName">Password</p>
+                        <p className="formInputAlternative">Forgot Password?</p>
+                    </div>
+                    <div className="customInput">
+                        {lockIcon}
+                        <input type="password" name="password" placeholder="••••••••"/>
+                    </div>
+                </div>
+                <label className="keepLoggedIn">
+                    <input type="checkbox" name="rememberMe" />
+                    <span>Keep me logged in</span>
+                </label>
+
+                <button className="formBtn">
+                    Log In {arrowRight}
+                </button>
+            </div>
         </form>
     );
 }
