20cba5c27e00224728e41e2af17acac2c73e2e40
Aleksandar Radojicic <aleksandarradojicic0@gmail.com>
2026-06-03 20:43:40 +0200

    Profile Update

--- studyflow/app/profile/Profile.tsx
+++ studyflow/app/profile/Profile.tsx
@@ -33,18 +33,20 @@ export function Profile(){
                         </button>
                     </div>
                 </div>
-                <div className="personalBio">
-                    <div className="flex gap-2.5 items-center justify-left">
-                        {quote}
-                        <h1>PERSONAL BIO</h1>
-                    </div>
-                    <p className="bioText">
-                        "Aspiring machine learning engineer with a focus on ethical AI. Currently balancing my final semester projects with a part-time internship at TechFlow."
-                    </p>
-                    <div className="tags flex gap-2">
-                        <p>#AI</p>
-                        <p>#Python</p>
-                        <p>#Ethics</p>
+                <div className="flex items-center justify-center px-5!">
+                    <div className="personalBio">
+                        <div className="flex gap-2.5 items-center justify-left">
+                            {quote}
+                            <h1>PERSONAL BIO</h1>
+                        </div>
+                        <p className="bioText">
+                            "Aspiring machine learning engineer with a focus on ethical AI. Currently balancing my final semester projects with a part-time internship at TechFlow."
+                        </p>
+                        <div className="tags flex gap-2">
+                            <p>#AI</p>
+                            <p>#Python</p>
+                            <p>#Ethics</p>
+                        </div>
                     </div>
                 </div>
             </div>
