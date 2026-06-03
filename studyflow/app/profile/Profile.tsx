06473833dfcafdcfd0837dd7aef5193d78c6a323
Aleksandar Radojicic <aleksandarradojicic0@gmail.com>
2026-06-03 19:32:52 +0200

    profile details and bio

--- studyflow/app/profile/Profile.tsx
+++ studyflow/app/profile/Profile.tsx
@@ -1,9 +1,43 @@
 import { Link } from "react-router";
+import { calendar, quote, uni } from "~/constants";
 
 export function Profile(){
     return(
         <main className="workPageMain">
-            
+            <div className="profileHolder">
+                <div className="profileInfo">
+                    <div className="flex items-center gap-5">
+                        <img src="/images/profile.png" alt="" />
+                        <div>
+                            <p>John Doe</p>
+                            <div className="flex h-[50px] bg-amber-50">
+                                <div className="flex flex-col items-center gap-1  h-[50px] justify-between">
+                                    {uni}
+                                    {calendar}
+                                </div>
+                                <div className="flex flex-col items-center gap-1 h-[50px] justify-between">                         
+                                    <p className="text-(--text-light)">Univerzitet u Beogradu</p>
+                                    <p className="text-(--text-light) text-[14px] font-medium">Joined Sept 2021</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="personalBio">
+                    <div className="flex gap-2.5 items-center justify-left">
+                        {quote}
+                        <h1>PERSONAL BIO</h1>
+                    </div>
+                    <p className="bioText">
+                        "Aspiring machine learning engineer with a focus on ethical AI. Currently balancing my final semester projects with a part-time internship at TechFlow."
+                    </p>
+                    <div className="tags flex gap-2">
+                        <p>#AI</p>
+                        <p>#Python</p>
+                        <p>#Ethics</p>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
